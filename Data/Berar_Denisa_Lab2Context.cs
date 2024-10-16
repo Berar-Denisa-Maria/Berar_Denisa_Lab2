185a00d15aab28c6117cc71010df0f5528d4fd04
Berar-Denisa-Maria <denisa.berar@stud.ubbcluj.ro>
2024-10-16 15:13:05 +0300

    Add project files.

---
+++ Data/Berar_Denisa_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Berar_Denisa_Lab2.Models;
+
+namespace Berar_Denisa_Lab2.Data
+{
+    public class Berar_Denisa_Lab2Context : DbContext
+    {
+        public Berar_Denisa_Lab2Context (DbContextOptions<Berar_Denisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Berar_Denisa_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Berar_Denisa_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Berar_Denisa_Lab2.Data;
+using Berar_Denisa_Lab2.Models;
+
+namespace Berar_Denisa_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Berar_Denisa_Lab2.Data.Berar_Denisa_Lab2Context _context;
+
+        public IndexModel(Berar_Denisa_Lab2.Data.Berar_Denisa_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
